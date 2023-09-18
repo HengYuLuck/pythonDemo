30d5b60ca849f33242005311d1b543e4d651cd3c
HengYu <hengyu@ailinking.com>
2023-09-18 19:26:26 +0800

    完善接口返回值

--- python/build/build.py
+++ python/build/build.py
@@ -16,7 +16,12 @@ table_data = [
 def get_table_data():
     # 在实际应用中，这里通常会查询数据库或其他数据源来获取表格数据
     # 这里我们直接返回示例数据
-    return jsonify(table_data)
+    response_data = {
+        "code": 200,  # 成功的响应码
+        "msg": "Success",  # 成功的消息
+        "data": table_data  # 表格数据作为 data 字段
+    }
+    return jsonify(response_data)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)  # 添加port参数来指定端口为8080
+    app.run(debug=True, port=8083)  # 将端口更改为8083
