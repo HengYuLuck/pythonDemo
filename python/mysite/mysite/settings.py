4640bf4a2e45db925c2af27a43fecd9a888d8571
HengYu <hengyu@ailinking.com>
2023-09-27 19:25:30 +0800

    update mySite

--- python/mysite/mysite/settings.py
+++ python/mysite/mysite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',  # MySQL 服务器主机名
+        'PORT': '3306',  # MySQL 服务器端口号
     }
 }
 
